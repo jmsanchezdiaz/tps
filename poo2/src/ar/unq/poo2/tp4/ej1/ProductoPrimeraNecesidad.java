553335a8df459436796a2940e39101c57c7d624a
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2022-04-20 16:21:38 -0300

    ej 2 modified -> ej 1 and tests

--- poo2/src/ar/unq/poo2/tp4/ej1/ProductoPrimeraNecesidad.java
+++ poo2/src/ar/unq/poo2/tp4/ej1/ProductoPrimeraNecesidad.java
@@ -2,8 +2,15 @@ package ar.unq.poo2.tp4.ej1;
 
 public class ProductoPrimeraNecesidad extends Producto{
 	
-	public ProductoPrimeraNecesidad(String nombre, double precio) {
-		super(nombre, precio * 0.9);
+	/**
+	 * @return una Instancia de Producto
+	 * @author juanmasd11
+	 * @param nombre del producto
+	 * @param precio base del producto, acepta decimales.
+	 * @param descuento, el descuento a aplicar en un rango de 1 a 100%.
+	 */
+	public ProductoPrimeraNecesidad(String nombre, double precio, double descuento) {
+		super(nombre, precio * (1 - (descuento / 100)));
 	}
 	
 	/**
@@ -12,8 +19,9 @@ public class ProductoPrimeraNecesidad extends Producto{
 	 * @param nombre del producto
 	 * @param precio base del producto, acepta decimales.
 	 * @param esPrecioCuidado, booleano para saber si es precio cuidado.
+	 * @param descuento, el descuento a aplicar en un rango de 1 a 100%.
 	 */
-	public ProductoPrimeraNecesidad(String nombre, double precio, boolean esPrecioCuidado) {
-		super(nombre, precio * 0.9, esPrecioCuidado);
+	public ProductoPrimeraNecesidad(String nombre, double precio, boolean esPrecioCuidado, double descuento) {
+		super(nombre,precio * (1 - (descuento / 100)) , esPrecioCuidado);
 	}
 }
