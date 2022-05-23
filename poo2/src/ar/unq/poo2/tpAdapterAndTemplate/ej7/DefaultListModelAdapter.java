9290ae08a2b23ab508d2990d08795c3ede17cd91
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2022-05-23 17:05:51 -0300

    fixed tp ej 7

--- poo2/src/ar/unq/poo2/tpAdapterAndTemplate/ej7/DefaultListModelAdapter.java
+++ poo2/src/ar/unq/poo2/tpAdapterAndTemplate/ej7/DefaultListModelAdapter.java
@@ -1,24 +1,36 @@
 package ar.unq.poo2.tpAdapterAndTemplate.ej7;
 
 import javax.swing.DefaultListModel;
+import javax.swing.ListModel;
+import javax.swing.event.ListDataListener;
 
-public class DefaulListModelAdapter extends DefaultListModel<String> {
+public class DefaultListModelAdapter<E> extends DefaultListModel<String>  {
 
 	private ListaDePalabrasOrdenadas list;
 	private static final long serialVersionUID = -3519925679910054901L;
 
-	public DefaulListModelAdapter(ListaDePalabrasOrdenadas list){
+	public DefaultListModelAdapter(ListaDePalabrasOrdenadas list){
 		this.list = list;
 	}
 	
 	@Override
 	public void addElement(String element) {
 		this.list.agregarPalabra(element);
+		
 	}
 	
 	@Override
-	public String get(int index) {
+	public String getElementAt(int index) {
 		return this.list.getPalabraDePosicion(index);
 	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return this.list.cantidadDePalabras();
+	}
+
+	
+	
 	
 }
---
+++ poo2/src/ar/unq/poo2/tpAdapterAndTemplate/ej7/Ventana.java
@@ -0,0 +1,38 @@
+package ar.unq.poo2.tpAdapterAndTemplate.ej7;
+
+import java.awt.Rectangle;
+
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+public class Ventana extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1321279402226181433L;
+
+	public Ventana(DefaultListModel<String> listModel){
+		this.setLayout(new BoxLayout(this.getContentPane(),BoxLayout.Y_AXIS));
+		this.setBounds(new Rectangle(180,180));
+		this.setVisible(true);
+		JLabel label=new JLabel("Lista de palabras");
+		label.setBounds(new Rectangle(40,40));
+		this.add(label);
+		listModel.addElement("casa");
+		listModel.addElement("arbol");
+		listModel.addElement("perro");
+		listModel.addElement("telefono");
+		listModel.addElement("brazo");
+		JList<String> lista=new JList<String>(listModel);
+		lista.setBounds(new Rectangle(110,160));
+		this.add(lista);
+		}
+	
+	public static void main(String[] args) {
+		new Ventana(new DefaultListModelAdapter<String>(new ListaDePalabrasOrdenadas()));
+		/*esta línea puede modificarse luego de resolver el ejercicio.*/
+	}
+}
